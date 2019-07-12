6b85192356747c6e6f8ecac5dde39b5faaf9ea24
Pete Goddard <pete@artofsoftware.co.uk>
2019-07-12 17:26:40 +0100

    Add second service for gif conversion

--- conversion_service/spec/integration/conversion_spec.rb
+++ conversion_service/spec/integration/conversion_spec.rb
@@ -2,8 +2,11 @@ require 'rails_helper'
 
 describe 'image conversion' do
 
-  context 'when the input file is jpg' do
-    ENV['CONVERT_FORMAT'] = 'png'
+  context 'when the service is running in png mode' do
+    before do
+      ENV['CONVERT_FORMAT'] = 'png'
+    end
+
     it 'converts the image' do
       request_body = {
         image_format: 'jpg',
@@ -14,6 +17,24 @@ describe 'image conversion' do
       result = JSON.parse(response.body)
       expect(result['image_format']).to eq('png')
       expect(result['base64_encoded_data'].length).to eq(read_test_image_as_base64('icon.png').length)
+    end
+  end
+
+  context 'when the service is running in gif mode' do
+    before do
+      ENV['CONVERT_FORMAT'] = 'gif'
+    end
+
+    it 'converts the image' do
+      request_body = {
+        image_format: 'jpg',
+        base64_encoded_data: read_test_image_as_base64('icon.jpg')
+      }
+      post '/api/convert', params: request_body
+      expect(response.status).to eq(200)
+      result = JSON.parse(response.body)
+      expect(result['image_format']).to eq('gif')
+      expect(result['base64_encoded_data'].length).to eq(read_test_image_as_base64('icon.gif').length)
     end
   end
 
